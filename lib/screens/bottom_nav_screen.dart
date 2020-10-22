5ee11898773deb9a7f83286815f537f1d949989f
manish <manishkumar.194ca024@nitk.edu.in>
2020-10-22 10:53:54 +0530

    Ui and email and call now problem is solved

---
+++ lib/screens/bottom_nav_screen.dart
@@ -0,0 +1,69 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_covid_dashboard_ui/screens/screens.dart';
+
+class BottomNavScreen extends StatefulWidget {
+  @override
+  _BottomNavScreenState createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  // Bottom Navigation items
+  final List _screens = [
+    HomeScreen(),
+    StatsScreen(),
+    Scaffold(),
+    Scaffold(),
+  ];
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedFontSize: 10,
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        //selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.pink,
+        elevation: 0.0,
+        items:
+            [Icons.home, Icons.insert_chart, Icons.local_hospital, Icons.info]
+                .asMap()
+                .map((key, value) => MapEntry(
+                      key,
+                      BottomNavigationBarItem(
+                        activeIcon: Icon(
+                          value,
+                          color: Colors.green,
+                          size: 28,
+                        ),
+                        title: Text(''),
+                        icon: Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 4.0,
+                            horizontal: 15.0,
+                          ),
+                          decoration: BoxDecoration(
+                            color: _currentIndex == key
+                                ? Colors.black
+                                : Colors.transparent,
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: Icon(value),
+                        ),
+                      ),
+                    ))
+                .values
+                .toList(),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_covid_dashboard_ui/config/palette.dart';
+
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      flexibleSpace: Image(
+        image: AssetImage('assets/images/app_bg4.jpg'),
+        fit: BoxFit.cover,
+      ),
+      elevation: 0.0,
+      leading: IconButton(
+        icon: const Icon(Icons.menu),
+        iconSize: 28.0,
+        onPressed: () {},
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.notifications_none),
+          iconSize: 28.0,
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
